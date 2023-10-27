520e946ceee2a8d36813ed259e69fcf5fbf101b7
Seokje <yonunim@gmail.com>
2023-10-27 17:53:40 +0900

    refactor: seperate forward

--- model.py
+++ model.py
@@ -16,11 +16,14 @@ class CNNModel(nn.Module):
         self.fc2 = nn.Linear(128, 1)
         
     def forward(self, x):
-        x = F.relu(self.conv1(x))
+        x = self.conv1(x)
+        x = F.relu(x)
         x = F.max_pool2d(x, kernel_size=2)
-        x = F.relu(self.conv2(x))
+        x = self.conv2(x)
+        x = F.relu(x)
         x = F.max_pool2d(x, kernel_size=2)
-        x = F.relu(self.conv3(x))
+        x = self.conv3(x)
+        x = F.relu(x)
         x = self.dropout1(x)
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
