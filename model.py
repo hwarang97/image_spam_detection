fabb1e27b844c2743b265b44732026c385b8a6b9
Seokje <yonunim@gmail.com>
2023-10-27 17:48:47 +0900

    feat: add model

---
+++ model.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(self).__init__()
+        
+        # Define the layers
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.dropout1 = nn.Dropout(0.1)
+        self.fc1 = nn.Linear(128 * 19 * 19, 128)
+        self.dropout2 = nn.Dropout(0.1)
+        self.fc2 = nn.Linear(128, 1)
+        
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, kernel_size=2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, kernel_size=2)
+        x = F.relu(self.conv3(x))
+        x = self.dropout1(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.dropout2(x)
+        x = torch.sigmoid(self.fc2(x))
+        return x
