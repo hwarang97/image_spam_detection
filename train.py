9305ab692c2cb17b2a56eb3c40a3430cdac4d255
root <root@DESKTOP-9MPN88P.localdomain>
2023-10-28 14:54:56 +0900

    feat: add f1 score matrics

--- train.py
+++ train.py
@@ -23,8 +23,8 @@ for epoch in range(num_epochs):
     # Training
     model.train()
     train_loss = 0.0
-    for images in train_loader:
-        images = images.to(device)
+    for images, labels in train_loader:
+        images = images.to(device), labels.to(device)
 
         optimizer.zero_grad()
         outputs = model(images)
@@ -38,15 +38,21 @@ for epoch in range(num_epochs):
     # valdiataion
     model.eval()
     valid_loss = 0.0
+    valid_preds, valid_targets = [], []
     with torch.no_grad():
         for images, labels in valid_loader:
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             loss = criterion(outputs, labels)
             valid_loss += loss.item() * images.size(0)
+            _, predicted = torch.max(outputs, 1)
+            valid_preds.extend(predicted.cpu().numpy())
+            valid_targets.extend(labels.cpu().numpy())
 
         valid_loss = valid_loss / len(valid_loader.dataset)
+        f1 = f1_score(valid_targets, valid_preds, average='binary')
 
     print(f"Epoch {epoch+1}/{num_epochs}, 
-        Train Loss: {train_loss:.4f}, 
-        Valid Loss: {valid_loss:.4f}")
+          Train Loss: {train_loss:.4f}, 
+          Valid Loss: {valid_loss:.4f}, 
+          F1 Score: {f1:.4f}")
