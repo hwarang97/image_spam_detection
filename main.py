c27507199ff55d116e7f52f58989e1730739f2b8
root <root@DESKTOP-9MPN88P.localdomain>
2023-10-28 19:09:36 +0900

    feat: add main func

--- main.py
+++ main.py
@@ -9,8 +9,22 @@ from test import test_model
 laerning_rate = 0.0001
 num_epochs = 10
 device = torch.device('cuda' if torch.cuda.is_avalilable() else 'cpu')
+batch_size = 32
 save_interval = 1
 
-# load data
-train_loader = get_loaders('asdf')
-valid_loader = get_loaders('asdf')
+def main():
+    # load data
+    train_data, val_data, test_data = split_dataset()
+    train_loader, val_loader, test_loader = get_loaders(train_data, val_data, test_data, batch_size=batch_size)
+
+    # init modle
+    model = CNNModel().to(device)
+
+    # train, val
+    train_model(model, train_loader, val_loader, num_epochs, laerning_rate, device)
+
+    # test
+    test_model(model, test_loader, device)
+
+if __name__ == '__main__':
+    main()
