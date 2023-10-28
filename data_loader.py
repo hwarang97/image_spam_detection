c893e6690568724076e7e79b79c4d4c15e843861
root <root@DESKTOP-9MPN88P.localdomain>
2023-10-28 10:49:09 +0900

    feat: add dataloader and test case

--- data_loader.py
+++ data_loader.py
@@ -18,6 +18,10 @@ class ImageDataset(Dataset):
     def __getitem__(self, idx):
         img_path = os.path.join(self.image_dir, self.image_list[idx])
         image = cv2.imread(img_path)
+
+        if image is None:
+            raise ValueError(f'Failed to load {img_path}')
+
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         if self.transform:
@@ -25,3 +29,24 @@ class ImageDataset(Dataset):
             image = augmented['image']
         
         return image
+    
+def get_loaders(image_dir, batch_size=2):
+    transform = A.Compose([
+        A.Resize(76, 76),
+        A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ToTensorV2()
+    ])
+
+    dataset = ImageDataset(image_dir=image_dir, transform=transform)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    return loader
+    
+# test code
+image_dir = "/mnt/c/Users/Kim Seok Je/Desktop/대학원/데이터보안과 프라이버시/report/personal_image_ham/personal_image_ham"
+dataset = ImageDataset(image_dir)
+print(len(dataset))
+
+loader = get_loaders(image_dir)
+for images in loader:
+    print(images.shape)
