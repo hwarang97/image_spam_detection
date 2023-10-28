27b4d2adb02f2c2f9651e81f3259eae3589d4e61
root <root@DESKTOP-9MPN88P.localdomain>
2023-10-28 11:22:44 +0900

    fix: can acess .gif, filter problematic images and remove cvtColor

--- data_loader.py
+++ data_loader.py
@@ -3,6 +3,8 @@ import cv2
 import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from PIL import Image
+import numpy as np
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
@@ -12,17 +14,34 @@ class ImageDataset(Dataset):
         self.image_list = os.listdir(image_dir)
         self.transform = transform
 
+        # filer out problematic iamge
+        self.image_list = [img for img in self.image_list
+                           if self._is_valid_image(os.path.join(image_dir, img))]
+
+    def _is_valid_image(self, img_path):
+        try:
+            with Image.open(img_path) as image:
+                image = np.array(image)
+            return True
+        except:
+            return False
+
     def __len__(self):
         return len(self.image_list)
     
     def __getitem__(self, idx):
         img_path = os.path.join(self.image_dir, self.image_list[idx])
-        image = cv2.imread(img_path)
+        # img_path = os.path.join(self.image_dir, '43A4F70B.1010101@seas.upenn.edu_unchecked.png')
 
-        if image is None:
-            raise ValueError(f'Failed to load {img_path}')
+        # open image with PIL
+        with Image.open(img_path) as image:
+            if image.mode != 'RGB':
+                image = image.convert('RGB')
+            image = np.array(image)
 
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # if grayscale, convert to RGB
+        if len(image.shape)==2:
+            image = np.stack([image, image, image], axis=-1)
 
         if self.transform:
             augmented = self.transform(image=image)
@@ -30,7 +49,7 @@ class ImageDataset(Dataset):
         
         return image
     
-def get_loaders(image_dir, batch_size=2):
+def get_loaders(image_dir, batch_size=32):
     transform = A.Compose([
         A.Resize(76, 76),
         A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -42,11 +61,16 @@ def get_loaders(image_dir, batch_size=2):
 
     return loader
     
-# test code
-image_dir = "/mnt/c/Users/Kim Seok Je/Desktop/대학원/데이터보안과 프라이버시/report/personal_image_ham/personal_image_ham"
-dataset = ImageDataset(image_dir)
-print(len(dataset))
+if __name__ == '__main__':
+    loader = get_loaders('"/mnt/c/Users/Kim Seok Je/Desktop/대학원/데이터보안과 프라이버시/report/personal_image_ham/personal_image_ham"')
+    for images in loader:
+        print(images.shape)
 
-loader = get_loaders(image_dir)
-for images in loader:
-    print(images.shape)
+# test code
+# image_dir = "/mnt/c/Users/Kim Seok Je/Desktop/대학원/데이터보안과 프라이버시/report/personal_image_ham/personal_image_ham"
+# dataset = ImageDataset(image_dir)
+# print(len(dataset))
+
+# loader = get_loaders(image_dir)
+# for images in loader:
+#     print(images.shape)
