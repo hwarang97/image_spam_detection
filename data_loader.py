603bf31cea6cb9cf9f692487c757daf58512b83f
root <root@DESKTOP-9MPN88P.localdomain>
2023-10-28 18:46:40 +0900

    chore: correct typo

--- data_loader.py
+++ data_loader.py
@@ -19,23 +19,38 @@ def split_dataset(folder_path, label, val_size=0.1, test_size=0.1):
 
     # assign label
     train_data = [(path, label) for path in train_data]
-    val_dat = [(path, label) for path in val_data]
+    val_data = [(path, label) for path in val_data]
     test_data = [(path, label) for path in test_data]
 
     return train_data, val_data, test_data
 
 # path
-spam_folder = "/mnt/c/Users/Kim Seok Je/Desktop/대학원/데이터보안과 프라이버시/report/personal_image_ham/personal_image_spam"
+spam_folder = "/mnt/c/Users/Kim Seok Je/Desktop/대학원/데이터보안과 프라이버시/report/personal_image_spam/personal_image_spam"
 ham_folder = "/mnt/c/Users/Kim Seok Je/Desktop/대학원/데이터보안과 프라이버시/report/personal_image_ham/personal_image_ham"
 
 # split data
 spam_train, spam_val, spam_test = split_dataset(spam_folder, label=1)
 ham_train, ham_val, ham_test = split_dataset(ham_folder, label=0)
 
+# test code
+# print(f' spam train len: {len(spam_train)}')
+# print(f' spam val len: {len(spam_val)}')
+# print(f' spam test len: {len(spam_test)}')
+# print(f' ham train len: {len(ham_train)}')
+# print(f' ham val len: {len(ham_val)}')
+# print(f' ham test len: {len(ham_test)}')
+
 # combine spam and ham
 train_data = spam_train + ham_train
 val_data = spam_val + ham_val
 test_data = spam_test + ham_test
+
+# testcode
+# print(f' train len: {len(train_data)}')
+# print(f' val len: {len(val_data)}')
+# print(f' test len: {len(test_data)}')
+for item in val_data:
+    print(item[0], item[1])
 
 # shuffle data
 np.random.shuffle(train_data)
@@ -43,14 +58,17 @@ np.random.shuffle(val_data)
 np.random.shuffle(test_data)
 
 class ImageDataset(Dataset):
-    def __init__(self, image_dir, transform=None):
-        self.image_dir = image_dir
-        self.image_list = os.listdir(image_dir)
-        self.transform = transform
+    def __init__(self, data_list, transform=None):
 
-        # filer out problematic iamge
-        self.image_list = [img for img in self.image_list
-                           if self._is_valid_image(os.path.join(image_dir, img))]
+        '''
+        Args:
+        - data_list (list of tuples): List of (image_path, label)
+        - transform (callable, optional): Optional transform to be applied on an image.
+        '''
+        
+        # filter problematic image
+        self.data_list = [item for item in data_list if self._is_valid_image(item[0])]
+        self.transform = transform
 
     def _is_valid_image(self, img_path):
         try:
@@ -61,10 +79,11 @@ class ImageDataset(Dataset):
             return False
 
     def __len__(self):
-        return len(self.image_list)
+        return len(self.data_list)
     
     def __getitem__(self, idx):
-        img_path = os.path.join(self.image_dir, self.image_list[idx])
+        img_path, label = self.data_list[idx]
+        # img_path = os.path.join(self.image_dir, self.image_list[idx])
         # img_path = os.path.join(self.image_dir, '43A4F70B.1010101@seas.upenn.edu_unchecked.png')
 
         # open image with PIL
@@ -81,24 +100,31 @@ class ImageDataset(Dataset):
             augmented = self.transform(image=image)
             image = augmented['image']
         
-        return image
+        return image, label
     
-def get_loaders(image_dir, batch_size=32):
+def get_loaders(train_data, val_data, test_data, batch_size=32):
     transform = A.Compose([
         A.Resize(76, 76),
         A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ToTensorV2()
     ])
 
-    dataset = ImageDataset(image_dir=image_dir, transform=transform)
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    train_dataset = ImageDataset(train_data, transform=transform)
+    val_dataset = ImageDataset(val_data, transform=transform)
+    test_dataset = ImageDataset(test_data, transform=transform)
 
-    return loader
-    
-if __name__ == '__main__':
-    loader = get_loaders('"/mnt/c/Users/Kim Seok Je/Desktop/대학원/데이터보안과 프라이버시/report/personal_image_ham/personal_image_ham"')
-    for images in loader:
-        print(images.shape)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+    return train_loader, val_loader, test_loader
+
+train_loader, val_loader, test_loader = get_loaders(train_data, val_data, test_data)
+
+# if __name__ == '__main__':
+#     loader = get_loaders('"/mnt/c/Users/Kim Seok Je/Desktop/대학원/데이터보안과 프라이버시/report/personal_image_ham/personal_image_ham"')
+#     for images in loader:
+#         print(images.shape)
 
 # test code
 # image_dir = "/mnt/c/Users/Kim Seok Je/Desktop/대학원/데이터보안과 프라이버시/report/personal_image_ham/personal_image_ham"
