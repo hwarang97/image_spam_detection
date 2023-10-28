574cb68a9ef83717e926994d0ff7a4a879f4f8e6
root <root@DESKTOP-9MPN88P.localdomain>
2023-10-28 09:37:18 +0900

    feat: add custom dataset class

--- data_loader.py
+++ data_loader.py
@@ -5,3 +5,23 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+
+class ImageDataset(Dataset):
+    def __init__(self, image_dir, transform=None):
+        self.image_dir = image_dir
+        self.image_list = os.listdir(image_dir)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_list)
+    
+    def __getitiem__(self, idx):
+        img_path = os.path.join(self.image_dir, self.image_list[idx])
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        if self.transform:
+            augmented = self.transform(image=image)
+            image = augmented['image']
+        
+        return image
