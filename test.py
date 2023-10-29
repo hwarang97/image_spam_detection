d8f5e72e6a69b11cfab63c733b524e7b1f1139c1
root <root@DESKTOP-9MPN88P.localdomain>
2023-10-29 12:16:20 +0900

    refactor: add test_model func

--- test.py
+++ test.py
@@ -7,32 +7,33 @@ from sklearn.metrics import f1_score
 # hyper-parameters
 device = torch.device('cuda' if torch.cuda.is_avalilable() else 'cpu')
 
-# Load the model
-model = CNNModel().to(device)
-model.load_state_dict(torch.load('model_checkpoint.pth'))
-model.eval()
+def test_model(model, test_loader, device):
+    # Load the model
+    model = CNNModel().to(device)
+    model.load_state_dict(torch.load('model_checkpoint.pth'))
+    model.eval()
 
-# load test dataset
-test_loader = get_loaders('asdf')
+    # load test dataset
+    test_loader = get_loaders('asdf')
 
-# criterion
-criterion = nn.BCELoss()
+    # criterion
+    criterion = nn.BCELoss()
 
-# eval on test set
-test_loss = 0.0
-test_preds, test_targets = [], []
+    # eval on test set
+    test_loss = 0.0
+    test_preds, test_targets = [], []
 
-with torch.no_grad():
-    for images, labels in test_loader:
-        images, labels = images.to(device), labels.to(device)
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        test_loss += loss.item() * images.size(0)
-        _, predicted = torch.max(outputs, 1)
-        test_preds.extend(predicted.cpu().numpy())
-        test_targets.extend(labels.cpu().numpy())
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            test_loss += loss.item() * images.size(0)
+            _, predicted = torch.max(outputs, 1)
+            test_preds.extend(predicted.cpu().numpy())
+            test_targets.extend(labels.cpu().numpy())
 
-test_loss = test_loss / len(test_loader.dataset)
-f1 = f1_score(test_targets, test_preds, average='binary')
+    test_loss = test_loss / len(test_loader.dataset)
+    f1 = f1_score(test_targets, test_preds, average='binary')
 
-print(f"Test Loss: {test_loss:.4f}, F1 Score: {f1:.4f}")
+    print(f"Test Loss: {test_loss:.4f}, F1 Score: {f1:.4f}")
